ebd4cdfa15def8a10fd51f645d5ef3f7b6731ed0
mburleson <megan.burleson@gmail.com>
2022-03-01 15:36:20 -0500

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
     siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
+      title: "Megan Burleson",
     },
     plugins: [
-
-    ]
-}
+      "gatsby-plugin-image",
+      "gatsby-plugin-sharp",
+    ],
+  };
