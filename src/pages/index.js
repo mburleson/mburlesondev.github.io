21e2bd2c18a9b8de8fa4c82a1306163c26abc100
mburleson <megan.burleson@gmail.com>
2022-03-05 18:27:34 -0500

    adding slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Hero from '../components/hero'
 import Intro from '../components/intro'
 import ProjectsIntro from '../components/projects-intro'
 
+
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
