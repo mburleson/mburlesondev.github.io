a3be39dccac392b597e310f0f65fbf2d15fd3f8d
mburleson <megan.burleson@gmail.com>
2022-03-05 21:54:52 -0500

    added slider

--- src/components/slider.js
+++ src/components/slider.js
@@ -10,42 +10,114 @@ import { Pagination } from "swiper";
 
 import * as swiperStyles from './slider.module.css';
 
-const Slider = (width) => {
-  
+const Slider = () => {
   return (
     <section className={swiperStyles.swiperContainer}>
-      <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+      <Swiper 
+        pagination={pagination}
+        modules={[Pagination]}
+        
+      >
         <SwiperSlide>
           <div className={swiperStyles.swiperSlideContainer}>
             <div className={swiperStyles.slideLeft}>
-            <h2>Blog Design</h2>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+                <div className={swiperStyles.slideLeftContent}>
+                <h2>Blog Design</h2>
+                  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
               sed diam nonumy eirmod tempor invidunt ut labore et dolore 
               magna aliquyam erat, sed diam voluptua. 
               At vero eos et accusam et justo duo dolores 
               et ea rebum. Stet clita kasd gubergren, 
               no sea takimata sanctus est Lorem ipsum dolor sit.
-              </p>
-            </div>
+                  </p>
+                </div>
+              </div>
             <div className={swiperStyles.slideRight}>
             <StaticImage 
                src="../images/sliderplaceholder.png" 
                alt="code and creativity" 
                placeholder="blurred"
-               layout="fullWidth"
-              
+               layout="fixed"
+               width={450}    
             />
             </div>
           </div>
         </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>
+          <div className={swiperStyles.swiperSlideContainer}>
+            <div className={swiperStyles.slideLeft}>
+                <div className={swiperStyles.slideLeftContent}>
+                <h2>Blog Design</h2>
+                  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+              sed diam nonumy eirmod tempor invidunt ut labore et dolore 
+              magna aliquyam erat, sed diam voluptua. 
+              At vero eos et accusam et justo duo dolores 
+              et ea rebum. Stet clita kasd gubergren, 
+              no sea takimata sanctus est Lorem ipsum dolor sit.
+                  </p>
+                </div>
+              </div>
+            <div className={swiperStyles.slideRight}>
+            <StaticImage 
+               src="../images/sliderplaceholder.png" 
+               alt="code and creativity" 
+               placeholder="blurred"
+               layout="fixed"
+               width={450}    
+            />
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={swiperStyles.swiperSlideContainer}>
+            <div className={swiperStyles.slideLeft}>
+                <div className={swiperStyles.slideLeftContent}>
+                <h2>Blog Design</h2>
+                  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+              sed diam nonumy eirmod tempor invidunt ut labore et dolore 
+              magna aliquyam erat, sed diam voluptua. 
+              At vero eos et accusam et justo duo dolores 
+              et ea rebum. Stet clita kasd gubergren, 
+              no sea takimata sanctus est Lorem ipsum dolor sit.
+                  </p>
+                </div>
+              </div>
+            <div className={swiperStyles.slideRight}>
+            <StaticImage 
+               src="../images/sliderplaceholder.png" 
+               alt="code and creativity" 
+               placeholder="blurred"
+               layout="fixed"
+               width={450}    
+            />
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={swiperStyles.swiperSlideContainer}>
+            <div className={swiperStyles.slideLeft}>
+                <div className={swiperStyles.slideLeftContent}>
+                <h2>Blog Design</h2>
+                  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+              sed diam nonumy eirmod tempor invidunt ut labore et dolore 
+              magna aliquyam erat, sed diam voluptua. 
+              At vero eos et accusam et justo duo dolores 
+              et ea rebum. Stet clita kasd gubergren, 
+              no sea takimata sanctus est Lorem ipsum dolor sit.
+                  </p>
+                </div>
+              </div>
+            <div className={swiperStyles.slideRight}>
+            <StaticImage 
+               src="../images/sliderplaceholder.png" 
+               alt="code and creativity" 
+               placeholder="blurred"
+               layout="fixed"
+               width={450}    
+            />
+            </div>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </section>
   );
