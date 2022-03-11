65632d7adb1daf7dc64c642f3592aa282ef7f8ae
mburleson <megan.burleson@gmail.com>
2022-03-10 23:30:46 -0500

    added responsive nav

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,29 @@
-import * as React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'gatsby'
 import { StaticImage } from "gatsby-plugin-image"
 import { AnchorLink } from "gatsby-plugin-anchor-links"
 import * as headerStyles from './header.module.css'
 
 const Header = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
 
-  
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
+
   return (
     <header>
+      <nav>
       <div className={headerStyles.siteTitle}>
         <Link to="/">
           <StaticImage src="../images/mblogotype.png" 
@@ -28,34 +43,41 @@ const Header = () => {
             />
         </Link>
       </div>
-      <nav>
-       <ul className={headerStyles.navLinks}>
-       <li className={headerStyles.navLinkItem}>
-        <AnchorLink to="/#projects" title="projects">
-          <span className={headerStyles.navLinkText}>Projects</span>
-         </AnchorLink>
-       </li>
-       <li className={headerStyles.navLinkItem}>
-         <AnchorLink to="/#testimonials" title="projects">
-           <span className={headerStyles.navLinkText}>Testimonials</span>
-         </AnchorLink>
-       </li>
-       <li className={headerStyles.navLinkItem}>
-         <Link to="/about" className={headerStyles.navLinkText}>
-           About Me
-         </Link>
-       </li>
-{/*        <li className={headerStyles.navLinkItem}>
-         <Link to="/blog" className={headerStyles.navLinkText}>
-           Blog
-         </Link>
-       </li> */}
-       <li className={headerStyles.navLinkItem}>
-         <Link to="/contact" className={headerStyles.navLinkText}>
-           Contact
-         </Link>
-       </li>
-     </ul>
+      <button
+            className={headerStyles.navToggle}
+            onClick={() => setShowLinks(!showLinks)}
+          >
+            <FontAwesomeIcon icon={faBars} size='2x' />
+          </button>
+      <div className={headerStyles.linksContainer} ref={linksContainerRef}>
+        <ul className={headerStyles.navLinks} ref={linksRef}>
+        <li className={headerStyles.navLinkItem}>
+          <AnchorLink to="/#projects" title="projects">
+            <span className={headerStyles.navLinkText}>Projects</span>
+          </AnchorLink>
+        </li>
+        <li className={headerStyles.navLinkItem}>
+          <AnchorLink to="/#testimonials" title="projects">
+            <span className={headerStyles.navLinkText}>Testimonials</span>
+          </AnchorLink>
+        </li>
+        <li className={headerStyles.navLinkItem}>
+          <Link to="/about" className={headerStyles.navLinkText}>
+            About Me
+          </Link>
+        </li>
+  {/*        <li className={headerStyles.navLinkItem}>
+          <Link to="/blog" className={headerStyles.navLinkText}>
+            Blog
+          </Link>
+        </li> */}
+        <li className={headerStyles.navLinkItem}>
+          <Link to="/contact" className={headerStyles.navLinkText}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+     </div>
     </nav>
   </header>
 
