9b289e4ea715272f544b5afa7b963ea66ff50fb1
mburleson <megan.burleson@gmail.com>
2022-03-14 11:53:20 -0400

    gatsby cloud plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,7 @@ module.exports = {
       "gatsby-plugin-fontawesome-css",
       "gatsby-plugin-anchor-links",
       "gatsby-plugin-react-helmet",
+      "gatsby-plugin-gatsby-cloud",
     ],
+    
   };
