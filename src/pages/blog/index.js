bb8a42f64af001dd709caa99b894221170107a9e
mburleson <megan.burleson@gmail.com>
2022-05-01 17:39:31 -0400

    welcome blog post

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -37,7 +37,7 @@ const BlogPage = ({ data }) => {
 
 export const query = graphql`
   query {
-    allMdx(sort: {fields: frontmatter___date, order: ASC}) {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
         frontmatter {
           title
