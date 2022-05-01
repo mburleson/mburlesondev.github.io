5c88b882c1d0a8cb5a54525b6e7a62c8ea35f396
mburleson <megan.burleson@gmail.com>
2022-05-01 15:32:24 -0400

    blog sidebar styling

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -2,6 +2,10 @@ import * as React from 'react'
 import { Link, graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faInstagram, faTwitch, faTwitter, faPinterest } from '@fortawesome/free-brands-svg-icons'
+
 import BlogLayout from '../../components/bloglayout'
 import * as blogStyles from  '../../components/blogstyles.module.css'
 
@@ -31,7 +35,22 @@ const BlogPost = ({ data }) => {
         <GatsbyImage
           image={sidebarImage}
           alt="Megan Burleson"
+          className={blogStyles.sidebarImage}
+          
         />
+        <ul>
+        <li><Link to="https://www.facebook.com/CreativityandCode" rel="noreferrer" target="_blank" alt="CreativityandCode on Facebook"> <FontAwesomeIcon icon={faFacebook} size='2x' /></Link></li>
+        <li><Link to="https://twitter.com/CreativitynCode" target="_blank" rel="noreferrer" alt="CreativitynCode on Twitter"><FontAwesomeIcon icon={faTwitter} size='2x' /></Link></li>
+        <li><Link to="https://www.instagram.com/creativityandcode/" target="_blank" alt="CreativityandCode on Instagram" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} size='2x' /></Link></li>
+        <li><Link to="https://www.pinterest.com/creativityandcode" target="_blank" alt="CreativityandCode on Pinterest" rel="noreferrer"><FontAwesomeIcon icon={faPinterest} size='2x' /></Link></li>
+        <li><Link to="https://www.twitch.tv/creativityandcode" target="_blank" alt="CreativityandCode on Twitch" rel="noreferrer"><FontAwesomeIcon icon={faTwitch} size='2x' /></Link></li>
+        </ul>
+        <hr/>
+        <h2>about me</h2>
+        <p>Hello! My name is Megan Burleson, and I am a Designer and Developer residing in Michigan.
+          I spread happiness by combining code and creativity to design graphics and build websites, apps and digital products that are attractive, usable and accessible to everyone. </p>
+        <Link to="/about"><button>read more</button></Link>
+        <hr/>
         </section>
         </section>
     </BlogLayout>
