77bc3d8cc52ecf796297de5eaeb9586afdc92a6e
mburleson <megan.burleson@gmail.com>
2022-08-12 15:41:00 -0400

    tweaking og in SEO component

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -2,13 +2,14 @@ import React from "react"
 import { useSiteMetadata } from "../hooks/use-site-metadata"
 
 export const Seo = ({ title, description, location, pathname, children }) => {
-  const { title: defaultTitle, description: defaultDescription, image, siteUrl, twitterUsername } = useSiteMetadata()
+  const { title: defaultTitle, description: defaultDescription, icon, image, siteUrl, twitterUsername, } = useSiteMetadata()
 
   const seo = {
     title: title || defaultTitle,
     description: description || defaultDescription,
     location: location,
     image: `${siteUrl}${image}`,
+    icon:`${siteUrl}${icon}`,
     url: `${siteUrl}${pathname || ``}`,
     twitterUsername,
   }
@@ -24,6 +25,10 @@ export const Seo = ({ title, description, location, pathname, children }) => {
       <meta name="twitter:description" content={seo.description} />
       <meta name="twitter:image" content={seo.image} />
       <meta name="twitter:creator" content={seo.twitterUsername} />
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:type" content={seo.image} />
+      <meta property="og:url" content={seo.siteUrl} />
+      <meta property="og:image" content={seo.image} />
       <link rel="icon" content={seo.image} />
       {children}
     </>
