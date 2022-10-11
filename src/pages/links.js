513af4ad5e154bb21a74d636082478b602d4a840
mburleson <megan.burleson@gmail.com>
2022-10-11 15:38:31 -0400

    fixed and updated Links Page

--- src/pages/links.js
+++ src/pages/links.js
@@ -1,28 +1,11 @@
 import * as React from 'react'
-import SEO from '../components/seo'
-import Layout from '../components/layout'
-import { Link } from 'gatsby'
+import Seo from '../components/seo'
 import { StaticImage } from "gatsby-plugin-image"
 import * as linkStyles from '../components/links.module.css'
 
 const Links = () => {
   return (
-    <Layout>
       <section className={linkStyles.container}>
-      <h1>Freebies</h1>
-      <section className={linkStyles.linkSection}>
-        <Link to="https://unique-composer-7271.ck.page/a734f1c63a">
-        <StaticImage
-          src="../images/freesummerprintable.png"
-          alt="Megan Burleson"
-          layout="constrained"
-          placeholder="blurred"
-          quality={100}
-          className={linkStyles.freeimg}
-        />
-        </Link>
-        </section>
-        <hr></hr>
         <StaticImage
           src="../images/megangatsbysquare.jpg"
           alt="Megan Burleson"
@@ -36,13 +19,18 @@ const Links = () => {
         <h1>Megan Burleson</h1>
         <h2>Graphic Designer & Developer</h2>
         <p>Hello! My name is Megan Burleson and I spread happiness by combining code and creativity to design graphics and build websites, apps and digital products that are fun, usable and accessible to everyone. Basically, I like to create art and code fun things!</p>
+        <hr></hr>
+        <h1>Freebies</h1>
+      <section className={linkStyles.linkSection}>
+       <p>None Available Right Now</p>
+        </section>
+        <hr></hr>
     </section>
-    </Layout>
   )
 }
 
 export const Head = () => {
-  return <SEO title="Contact Me | Megan Burleson"/>;
+  return <Seo title="Links | Megan Burleson"/>;
   };
 
 export default Links
