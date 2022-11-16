a21c9c485cdcfebfdc6b424be51142d875841af1
mburleson <megan.burleson@gmail.com>
2022-11-15 19:44:27 -0500

    about me page noodling

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,7 +14,6 @@ const AboutPage = () => {
       <section className={aboutStyles.aboutSection}>
         <h1>About Me</h1>
         <div className={aboutStyles.aboutSectionTop}>
-        <h2>how my story began...</h2>
           <div className={aboutStyles.aboutSectionTopItem}>
             <p>I was raised with science fiction movies, books, and video games. So, it wasn’t a surprise to my parents when after getting my first computer when I was 5 – a Commodore 64 – I exclaimed that I was going to be a <span className={aboutStyles.pink}>graphic designer</span> when I grew up. I really liked <span className={aboutStyles.green}>Print Shop</span> and <span className={aboutStyles.yellow}>Color Me</span>. After drawing my way through high school, I went on to college to earn my degree in Graphic Design.</p>
            <StaticImage src="../images/c64.png" 
@@ -24,23 +23,33 @@ const AboutPage = () => {
              width={250}/>
             </div>
            <div className={aboutStyles.aboutSectionTopItem2}>
-           <p>After college, I started my career as a graphic designer by designing stationery for major retail outlets, and print materials for the corporate world, and then I became fascinated by web design and development. I fired up my left brain and taught myself a little bit of <span className={aboutStyles.pink}>HTML</span>, <span className={aboutStyles.purple}>CSS</span>, and <span className={aboutStyles.yellow}>JavaScript</span>.</p>
+           <p>After college, I started my career as a graphic designer by designing stationery for major retail outlets, and print materials for the corporate world, and then I became fascinated by web design and development. I fired up my left brain and taught myself <span className={aboutStyles.pink}>HTML</span>, <span className={aboutStyles.purple}>CSS</span>, and <span className={aboutStyles.yellow}>JavaScript</span>.</p>
            <StaticImage src="../images/books.png" 
             alt="megan burleson designer developer" 
             placeholder="blurred"
             layout="fixed"
             width={250}/>
         </div>
-        <h3>Now, I Spread Happiness with</h3>
+        <h2>Now, I spread happiness with</h2>
         <div className={aboutStyles.cncImageContainer}>
         <StaticImage src="../images/cnc2.png" 
             alt="creativity and code" 
             placeholder="blurred"
-            layout="fixed"
-            width={350}
+            layout="constrained"
+            width={550}
            />
         </div>       
         </div> 
+        <div className={aboutStyles.aboutSectionLearning}>
+       <h1>What I'm Up To Now</h1>
+            <ul>
+            <li className={aboutStyles.aboutSectionLearningItem}><FontAwesomeIcon className={aboutStyles.green} icon={faReact} size='2x' /><p>Working on various coding projects using JavaScript, and React frameworks.</p></li>
+            <li className={aboutStyles.aboutSectionLearningItem}><FontAwesomeIcon className={aboutStyles.purple} icon={faCube} size='2x' /><p>Currently, I am learning 3D modeling with Blender to use with 3D printing projects.</p></li>
+            <li className={aboutStyles.aboutSectionLearningItem}><FontAwesomeIcon className={aboutStyles.yellow} icon={faPallet} size='2x' /><p>I am also making a bunch of art, and posting it on Instagram.</p></li>
+            <li className={aboutStyles.aboutSectionLearningItem}><FontAwesomeIcon className={aboutStyles.pink} icon={faHeart} size='2x' /><p>I love what I do, and I keep myself up to date with current design and coding trends and practices on a regular basis.</p></li>
+            </ul>
+        </div>
+      </section>
         <div className={aboutStyles.aboutSectionSkills}>
           <h1>Skills</h1>
           <div className={aboutStyles.aboutSectionSkillsItem}>
@@ -61,23 +70,13 @@ const AboutPage = () => {
             placeholder="blurred"
             layout="fixed"
             width={50}/>
-            <h2 className={aboutStyles.pink}>graphic design</h2>
+            <h2 className={aboutStyles.pink}>Graphic Design</h2>
             <p>I use digital illustration tools including Adobe Illustrator and Photoshop to design graphic assets for use across a variety of multimedia that inspires and informs.</p>
             <p>Experience developing both digital / print design projects (brochures, logos, posters, infographics, advertisements)</p>
             <p>I am also adept in setting up files for production, photo editing and color correction.</p>
             </div>
             
         </div>
-        <div className={aboutStyles.aboutSectionLearning}>
-            <h1>Always Learning</h1>
-            <ul>
-            <li className={aboutStyles.aboutSectionLearningItem}><FontAwesomeIcon className={aboutStyles.green} icon={faReact} size='2x' /><p>Working on various coding projects using JavaScript, and React frameworks.</p></li>
-            <li className={aboutStyles.aboutSectionLearningItem}><FontAwesomeIcon className={aboutStyles.purple} icon={faCube} size='2x' /><p>Currently, I am learning 3D modeling with Blender to use with 3D printing projects.</p></li>
-            <li className={aboutStyles.aboutSectionLearningItem}><FontAwesomeIcon className={aboutStyles.yellow} icon={faPallet} size='2x' /><p>I am also making a bunch of art, and posting it on Instagram.</p></li>
-            <li className={aboutStyles.aboutSectionLearningItem}><FontAwesomeIcon className={aboutStyles.pink} icon={faHeart} size='2x' /><p>I love what I do, and I keep myself up to date with current design and coding trends and practices on a regular basis.</p></li>
-            </ul>
-        </div>
-      </section>
     </Layout>
   )
 }
